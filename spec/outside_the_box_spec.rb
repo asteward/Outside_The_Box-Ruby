0a5626b0bdc1c232d856296234d0949b1edc789e
Eric Johnson & Austin Steward <student@epicodus.com>
2014-07-25 12:03:32 -0700

    :square: Add box-drawing function

--- spec/outside_the_box_spec.rb
+++ spec/outside_the_box_spec.rb
@@ -6,4 +6,14 @@ describe 'Box' do
     new_box = Box.new(2, 3)
     new_box.should be_an_instance_of Box
   end
+
+  it 'returns "Square" for a square quadrilateral input' do
+    new_box = Box.new(2,2)
+    new_box.find_type.should eq "Square"
+  end
+
+  it 'returns "Rectangle" for a rectangular quadrilateral input' do
+    new_box = Box.new(4,8)
+    new_box.find_type.should eq "Rectangle"
+  end
 end
